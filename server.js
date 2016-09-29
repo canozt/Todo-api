010f98ce3c8892461066cdc898bda220cb69cc2a
canoz <canozturk0@gmail.com>
2016-09-29 16:28:59 -0400

    Add POST for todos

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express('');
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'go gym',
-	completed: false
-}, {
-	id: 2,
-	description: 'go office',
-	completed: false
-}, {
-	id: 3,
-	description: 'go home',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
 	res.send('Todo API Root');
@@ -30,7 +21,7 @@ app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo;
 
-	// Iterate of tidis array. Find the match
+	// Iterate of todos array. Find the match.
 	todos.forEach(function (todo) {
 		if(todoId === todo.id){
 			matchedTodo = todo;
@@ -42,8 +33,19 @@ app.get('/todos/:id', function (req, res) {
 	}else{
 		res.status(404).send();
 	}
-
 });
+
+// POST /todos/
+app.post('/todos', function(req, res){
+	var body = req.body;
+
+	body.id = todoNextId++;
+	todos.push(body);
+
+	res.json(body);
+});
+
+
 
 
 app.listen(PORT, function(){
