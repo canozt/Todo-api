d4794152b39bc3c1387351fddb6070f5249ed488
canoz <canozturk0@gmail.com>
2016-10-02 15:21:33 -0400

    Add todo model and post /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express('');
 var PORT = process.env.PORT || 3000;
@@ -50,7 +51,13 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function(req, res){
 	var body = _.pick(req.body, 'description', 'complete');
 
+	db.todo.create(body).then(function(todo){
+		res.json(todo.toJSON())
+	}, function(e) {
+		res.status(400).json(e);
+	});
 
+	/*
 	if(!_.isBoolean(body.complete) || !_.isString(body.description) || body.description.trim().length === 0){
 		return res.status(400).send();
 	}
@@ -60,7 +67,7 @@ app.post('/todos', function(req, res){
 	body.id = todoNextId++;
 	todos.push(body);
 
-	res.json(body);
+	res.json(body);*/
 });
 
 // DELETE /todos/:id
@@ -107,18 +114,9 @@ app.put('/todos/:id', function(req, res){
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT, function(){
-	console.log('listening port ' + PORT + '!');
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function(){
+		console.log('listening port ' + PORT + '!');
+	});
 });
+
